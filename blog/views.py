6ed11c9004b01b31c0ea5f8b37cd6f8919525081
Vitaliy <grak-v1998@mail.ru>
2023-02-26 00:24:30 +0300

    Price

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def price(request):
-    return render(request, 'price/price.html')
+    posts = Post.objects
+    return render(request, 'price/price.html', {'posts' : posts})
